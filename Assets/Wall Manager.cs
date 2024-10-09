b6e9e6fa9057f88664e7ecbed53e60ac26c689e9
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-10-09 10:47:37 -0300

    geração de mapa feito

--- Assets/Wall Manager.cs
+++ Assets/Wall Manager.cs
@@ -1,14 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WallManager : MonoBehaviour
 {
     public GameObject wall;
 
+    int[,] mapa;
+    public int linhas = 10;
+    public int colunas = 15;
+    public float tCelula = 1.1f;
+    float dCamera = 10f;
 
+    private void Awake()
+    {
+        mapa = new int[linhas, colunas];
+    }
     private void Start()
     {
+        GerarMapa();
         
     }
-}
+
+    void GerarMapa()
+    {
+        for (int A = 0; A < colunas; A++)
+        {
+            for (int L = 0; L < linhas; L++)
+            {
+                Instantiate(wall, new Vector2(L * tCelula, A * tCelula), Quaternion.identity);  
+            }
+        }
+        Vector3 centroMatriz = new Vector3((linhas - 1) * tCelula / 2, (colunas - 1) * tCelula / 2, -dCamera);
+        Camera.main.transform.position = centroMatriz;
+        Camera.main.orthographicSize = Mathf.Max(linhas, colunas) * tCelula / 2;
+    }
+} 
