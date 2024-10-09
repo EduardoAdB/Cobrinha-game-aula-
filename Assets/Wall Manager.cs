7bbd1a06677638d2e1e9adef3ad003fcb6433c62
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-10-09 10:27:22 -0300

    criação dos itens basicos da branch

---
+++ Assets/Wall Manager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallManager : MonoBehaviour
+{
+    public GameObject wall;
+
+
+    private void Start()
+    {
+        
+    }
+}
