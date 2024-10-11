f4f1ad2c2262e9edfe044ab227135353cdf1e350
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-10-11 19:24:51 -0300

    correção de script

--- Assets/Wall Manager.cs
+++ Assets/Wall Manager.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO.IsolatedStorage;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class WallManager : MonoBehaviour
+public class wallManager : MonoBehaviour
 {
-    public GameObject wall;
+    #region singleton
+    public static wallManager instance;
 
-    int[,] mapa;
+    #endregion
+
+    public GameObject wall;
+    public GameObject fruit;
+
+
+
+    public GameObject fruta;
+    public int quantidadeFrutas;
+
+    public int[,] mapa;
     public int linhas = 10;
     public int colunas = 15;
     public float tCelula = 1.1f;
@@ -17,11 +26,12 @@ public class WallManager : MonoBehaviour
     private void Awake()
     {
         mapa = new int[linhas, colunas];
+        instance = this;
     }
     private void Start()
     {
         GerarMapa();
-        
+        Frutas();
     }
 
     void GerarMapa()
@@ -30,11 +40,21 @@ public class WallManager : MonoBehaviour
         {
             for (int L = 0; L < linhas; L++)
             {
-                Instantiate(wall, new Vector2(L * tCelula, A * tCelula), Quaternion.identity);  
+                Instantiate(wall, new Vector2(L * tCelula, A * tCelula), Quaternion.identity);
+
             }
         }
         Vector3 centroMatriz = new Vector3((linhas - 1) * tCelula / 2, (colunas - 1) * tCelula / 2, -dCamera);
         Camera.main.transform.position = centroMatriz;
         Camera.main.orthographicSize = Mathf.Max(linhas, colunas) * tCelula / 2;
     }
-} 
+
+    public void Frutas()
+    {
+        float x = Random.Range(0, linhas) * tCelula;
+        float y = Random.Range(0, colunas) * tCelula;
+        Vector2 randomposition = new Vector2(x, y);
+        fruta = Instantiate(fruit, randomposition, Quaternion.identity);
+    }
+
+}
